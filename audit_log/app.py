310e58ac0caa99509283d0bf4f0e7ba5e551c6b8
karan-sohi <62645627+karan-sohi@users.noreply.github.com>
2021-11-03 23:43:45 -0700

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -83,6 +83,8 @@ def get_chocolate_cake_orders(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
+CORS(app.app)
+app.app.config['CORS_HEADERS']='Content-Type'
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
