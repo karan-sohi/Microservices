aa36c7bfcfd047a7f0d5cc2737933d557e243b1d
karan-sohi <62645627+karan-sohi@users.noreply.github.com>
2021-11-16 08:50:59 -0800

    Update EndpointAudit.js

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -5,7 +5,8 @@ export default function EndpointAudit(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [log, setLog] = useState(null);
     const [error, setError] = useState(null)
-	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+    const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+    const [index, setIndex] = useState(null);
 
     const getAudit = () => {
         fetch(`http://karankafka.eastus2.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
@@ -14,6 +15,7 @@ export default function EndpointAudit(props) {
 				console.log("Received Audit Results for " + props.endpoint)
                 setLog(result);
                 setIsLoaded(true);
+		setIndex(rand_val);
             },(error) =>{
                 setError(error)
                 setIsLoaded(true);
@@ -32,7 +34,7 @@ export default function EndpointAudit(props) {
         
         return (
             <div>
-                <h3>{props.endpoint}-{rand_val}</h3>
+                <h3>{props.endpoint}-{index}</h3>
                 {JSON.stringify(log)}
             </div>
         )
