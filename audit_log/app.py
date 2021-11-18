f84f3b895b132a3efcf6a5fc759d9566c3ca9e5e
karan-sohi <62645627+karan-sohi@users.noreply.github.com>
2021-11-18 10:29:44 -0800

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -6,7 +6,30 @@ import connexion
 from connexion import NoContent
 from pykafka import KafkaClient
 from flask_cors import CORS,cross_origin
+import os
 
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
+    app_conf = yaml.safe_load(f.read())
+
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+    
+logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
+    
 
 with open ('log_conf.yml', 'r') as f:
     log_config = yaml.safe_load(f.read())
