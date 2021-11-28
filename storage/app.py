f6c7874ab17e4c0d742a868920a62eb7de79d189
karan-sohi <62645627+karan-sohi@users.noreply.github.com>
2021-11-27 18:58:21 -0800

    Update app.py

--- storage/app.py
+++ storage/app.py
@@ -207,7 +207,7 @@ def process_messages():
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yml", base_path="/storage", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     t1 = Thread(target=process_messages)
