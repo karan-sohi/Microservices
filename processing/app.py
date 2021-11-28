bdeb1c6235849c45385e7994af9c2946b03130e7
karan-sohi <62645627+karan-sohi@users.noreply.github.com>
2021-11-27 19:03:36 -0800

    Update app.py

--- processing/app.py
+++ processing/app.py
@@ -172,8 +172,9 @@ def get_stats():
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test": 
+    CORS(app.app) 
+    app.app.config['CORS_HEADERS'] = 'Content-Type' 
 app.add_api("openapi.yml", base_path="/processing", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
