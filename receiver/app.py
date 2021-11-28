f7a6cb5861def9e03d289bc521e1cad75837a5fa
karan-sohi <62645627+karan-sohi@users.noreply.github.com>
2021-11-27 18:57:38 -0800

    Update app.py

--- receiver/app.py
+++ receiver/app.py
@@ -108,7 +108,7 @@ def order_chocolate_cake(body):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yml", base_path="/receiver", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     app.run(port=8080)
