52d358721fd4b1b25b8dc50c407435b6c150fe8e
karan-sohi <62645627+karan-sohi@users.noreply.github.com>
2021-11-27 19:02:43 -0800

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -16,6 +16,7 @@ else:
     print("In Dev Environment")
     app_conf_file = "app_conf.yml"
     log_conf_file = "log_conf.yml"
+    
 
 with open(app_conf_file, 'r') as f:
     app_conf = yaml.safe_load(f.read())
@@ -106,8 +107,9 @@ def get_chocolate_cake_orders(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test": 
+    CORS(app.app) 
+    app.app.config['CORS_HEADERS'] = 'Content-Type' 
 app.add_api("openapi.yml", base_path="/audit_log", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
